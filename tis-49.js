8dbb6ae2bd7e182d7b291cd99ab572c4c6bb9693
Aaron Broder <aaronbroder@gmail.com>
2015-06-03 03:24:20 -0700

    Added simple program execution.

--- tis-49.js
+++ tis-49.js
@@ -2,57 +2,89 @@ var T21 = {
   _r: { ACC: 0, BAK: 0, NIL: 0, PC: 0 },
   _p: { LEFT: null, RIGHT: null, UP: null, DOWN: null, LAST: null },
 
+  execute: function (program) {
+    T21._r.PC = 0;
+    // while (true) {
+      var line = program[T21._r.PC++];
+      line = line.split(" "); // TODO: more generous with formatting (extra whitespace)
+      T21[line[0]].apply(this, line);
+
+      // if (T21._r.PC >= program.length) T21._r.PC = 0;
+    // }
+  },
+
   NOP: function () {
+    _checkArguments(arguments, 1);
+
     T21.ADD(T21._r.NIL);
   },
 
-  MOV: function (src, dst) {
+  MOV: function () {
+    _checkArguments(arguments, 3);
 
   },
 
   SWP: function () {
+    _checkArguments(arguments, 1);
+
     var temp = T21._r.BAK;
     T21._r.BAK = T21._r.ACC;
     T21._r.ACC = temp;
   },
 
   SAV: function () {
+    _checkArguments(arguments, 1);
+
     T21._r.BAK = T21._r.ACC;
   },
 
-  ADD: function (src) {
+  ADD: function () {
+    _checkArguments(arguments, 2);
 
   },
 
-  SUB: function (src) {
+  SUB: function () {
+    _checkArguments(arguments, 2);
 
   },
 
   NEG: function () {
+    _checkArguments(arguments, 1);
+
     T21._r.ACC *= -1;
   },
 
-  JMP: function (label) {
+  JMP: function () {
+    _checkArguments(arguments, 2);
 
   },
 
-  JEZ: function (label) {
+  JEZ: function () {
+    _checkArguments(arguments, 2);
 
   },
 
-  JNZ: function (label) {
+  JNZ: function () {
+    _checkArguments(arguments, 2);
 
   },
 
-  JGZ: function (label) {
+  JGZ: function () {
+    _checkArguments(arguments, 2);
 
   },
 
-  JLZ: function (label) {
+  JLZ: function () {
+    _checkArguments(arguments, 2);
 
   },
 
-  JRO: function (src) {
+  JRO: function () {
+    _checkArguments(arguments, 2);
 
+  },
+
+  _checkArguments: function (arguments, count) {
+    if (arguments.length != count) throw T21._r.PC;
   }
 };
